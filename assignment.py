cfa56f645397343cf2c14ac9dd2641c026c6a104
Gopal <gopal@telepathyinfotech.com>
2025-04-17 23:18:30 +0530

    Working on python assignments

---
+++ assignment.py
@@ -0,0 +1,15 @@
+# Task 1 
+numberOne = input("Provide First Number : ");
+numberSecond = input("Provide Second Number : ");
+
+print(int(numberOne) + int(numberSecond));
+print(int(numberOne) - int(numberSecond));
+print(int(numberOne) * int(numberSecond));
+print(int(numberOne) / int(numberSecond));
+
+
+
+#Task 2
+firstName = input("Provide First Name : ");
+lastName = input("Provide Last Name : ");
+print("Hello, " + firstName + " " + lastName + "! Welcome to the python program.")
