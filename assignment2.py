c1faab818f96039268857dd3e091a57bfb97265e
Gopal <gopal@telepathyinfotech.com>
2025-04-21 19:33:29 +0530

    Working on Assignment two

--- assignment2.py
+++ assignment2.py
@@ -1,3 +1,19 @@
+#Task 1
+
+number = input("Provide me a number :")
+print(number)
+string = "This is odd number"
+if(int(number) % 2 == 0):
+    string = "This is even number"
+
+
+print(string)
+
+
+
+
+
+#Task 2
 sumValue = 0
 finalResult = 0
 for i in range(1,50):
