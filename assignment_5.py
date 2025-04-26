223371ccd75487fa2ee3dc22f2fba5120b1af6a5
Gopal <gopal@telepathyinfotech.com>
2025-04-26 12:41:38 +0530

    Assignment 5

--- assignment_5.py
+++ assignment_5.py
@@ -1,24 +1,24 @@
-# Task 1
-stuData = {"Gopal":'90',"Divya":'99',"Bharat":'100',"Vikarm":'98',"Namrta":'97',"Pooja":'35'}
-print(stuData)
-try:
-    Name = input("Enter the student's Name : ")
-    print(Name + "'s marks : "  + stuData[Name])
-except:
-    print("Student not found")
+# # Task 1
+# stuData = {"Gopal":'90',"Divya":'99',"Bharat":'100',"Vikarm":'98',"Namrta":'97',"Pooja":'35'}
+# print(stuData)
+# try:
+#     Name = input("Enter the student's Name : ")
+#     print(Name + "'s marks : "  + stuData[Name])
+# except:
+#     print("Student not found")
 
-# Task 1 Approch 2 Here is drowback is that I can not add more then one key:value into distionaries 
-stuData_2 = {"Gopal":'90',"Divya":'99',"Bharat":'100',"Vikarm":'98',"Namrta":'97',"Pooja":'35'}
-try:
-    sName = input("Enter the student's Name : ")
-    # print(stuData_2)
-    findData = sName in stuData_2
-    if(findData):
-        print(sName + "'s marks : "  + stuData_2[sName])
-    else:
-        print("Student not found")
-except:
-    print("Something went wrong !!!")
+# # Task 1 Approch 2 Here is drowback is that I can not add more then one key:value into distionaries 
+# stuData_2 = {"Gopal":'90',"Divya":'99',"Bharat":'100',"Vikarm":'98',"Namrta":'97',"Pooja":'35'}
+# try:
+#     sName = input("Enter the student's Name : ")
+#     # print(stuData_2)
+#     findData = sName in stuData_2
+#     if(findData):
+#         print(sName + "'s marks : "  + stuData_2[sName])
+#     else:
+#         print("Student not found")
+# except:
+#     print("Something went wrong !!!")
 
 
 
@@ -39,9 +39,15 @@ except:
 listData = [1,2,3,4,5,6,7,8,9,10]
 print("Original list :",listData) 
 sliceListData = listData[0:5]
-print("Extracted first five elements : ",sliceListData )
-reverseSliceListData = sliceListData.reverse()
 
-print(reverseSliceListData)  
+
+# The reason you see None is because the .reverse() method reverses the list in place and does not return a new list, it returns None. To fix it, just call sliceListData.reverse() first and then print sliceListData, or use slicing like sliceListData[::-1] to get a reversed copy.
+
+print("Extracted first five elements : ",sliceListData )
+# reverseSliceListData = sliceListData.reverse()
+# After help TuteDude Thanks !!!
+reverseSliceListData = sliceListData[::-1]
+
+print("Reversed Extracted elements : ", reverseSliceListData)  
 
 # I am not getting why None is coming as when we print sliceListData it shows [1,2,3,4,5]. Please help
