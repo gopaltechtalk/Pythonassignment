19e4c0a9a7ae3cf29da8b2b25dd99023e5cf33cf
Gopal <gopal@telepathyinfotech.com>
2025-04-26 12:42:56 +0530

    Assignment 5

--- assignment_5.py
+++ assignment_5.py
@@ -1,24 +1,24 @@
-# # Task 1
-# stuData = {"Gopal":'90',"Divya":'99',"Bharat":'100',"Vikarm":'98',"Namrta":'97',"Pooja":'35'}
-# print(stuData)
-# try:
-#     Name = input("Enter the student's Name : ")
-#     print(Name + "'s marks : "  + stuData[Name])
-# except:
-#     print("Student not found")
+# Task 1
+stuData = {"Gopal":'90',"Divya":'99',"Bharat":'100',"Vikarm":'98',"Namrta":'97',"Pooja":'35'}
+print(stuData)
+try:
+    Name = input("Enter the student's Name : ")
+    print(Name + "'s marks : "  + stuData[Name])
+except:
+    print("Student not found")
 
-# # Task 1 Approch 2 Here is drowback is that I can not add more then one key:value into distionaries 
-# stuData_2 = {"Gopal":'90',"Divya":'99',"Bharat":'100',"Vikarm":'98',"Namrta":'97',"Pooja":'35'}
-# try:
-#     sName = input("Enter the student's Name : ")
-#     # print(stuData_2)
-#     findData = sName in stuData_2
-#     if(findData):
-#         print(sName + "'s marks : "  + stuData_2[sName])
-#     else:
-#         print("Student not found")
-# except:
-#     print("Something went wrong !!!")
+# Task 1 Approch 2 Here is drowback is that I can not add more then one key:value into distionaries 
+stuData_2 = {"Gopal":'90',"Divya":'99',"Bharat":'100',"Vikarm":'98',"Namrta":'97',"Pooja":'35'}
+try:
+    sName = input("Enter the student's Name : ")
+    # print(stuData_2)
+    findData = sName in stuData_2
+    if(findData):
+        print(sName + "'s marks : "  + stuData_2[sName])
+    else:
+        print("Student not found")
+except:
+    print("Something went wrong !!!")
 
 
 
@@ -36,6 +36,7 @@
 
 
 # Task 2 
+
 listData = [1,2,3,4,5,6,7,8,9,10]
 print("Original list :",listData) 
 sliceListData = listData[0:5]
